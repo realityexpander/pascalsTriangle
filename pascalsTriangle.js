98b435bcfd4164d934967563bcbe70fc1577193f
Chris Athanas <realityexpanderdev@gmail.com>
2019-03-04 18:19:12 -0600

    Update pascalsTriangle.js

--- pascalsTriangle.js
+++ pascalsTriangle.js
@@ -4,7 +4,11 @@ function pascalsTriangle (row) {
   for (var i = 0; i < numRows; i++) { 
     elements[i] = new Array(i+1);
     
-
+    for (var j = 0; j < i+1; j++) {            
+      if (j === 0 || j === i) {
+        elements[i][j] = 1;
+      }
+    }
   }
   
   return elements;
